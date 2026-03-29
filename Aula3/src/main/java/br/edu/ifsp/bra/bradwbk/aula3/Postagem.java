343671db675ee5695ca5385ee5636f183d55f41d
PaulingCavalcante <ph.doamaralcavalcante@gmail.com>
2026-03-29 20:38:19 -0300

    feat: add Postagem model and PostagemController class

---
+++ Aula3/src/main/java/br/edu/ifsp/bra/bradwbk/aula3/Postagem.java
@@ -0,0 +1,36 @@
+package br.edu.ifsp.bra.bradwbk.aula3;
+
+import java.time.LocalDateTime;
+
+public class Postagem {
+    private Long id;
+    private String titulo;
+    private String conteudo;
+    private LocalDateTime dataCriacao;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getConteudo() {
+        return conteudo;
+    }
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+}
---
+++ Aula3/src/main/java/br/edu/ifsp/bra/bradwbk/aula3/PostagemController.java
@@ -0,0 +1,5 @@
+package br.edu.ifsp.bra.bradwbk.aula3;
+
+public class PostagemController {
+
+}
