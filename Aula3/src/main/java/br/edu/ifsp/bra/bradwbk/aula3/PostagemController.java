64f7796021fa5ede472dd053dc30226645a47063
PaulingCavalcante <ph.doamaralcavalcante@gmail.com>
2026-03-29 23:30:47 -0300

    fix: correct Java version in pom.xml and update PostagemController request mapping

--- Aula3/src/main/java/br/edu/ifsp/bra/bradwbk/aula3/PostagemController.java
+++ Aula3/src/main/java/br/edu/ifsp/bra/bradwbk/aula3/PostagemController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/postagens")
+@RequestMapping("/posts")
 public class PostagemController {
 
     private List<Postagem> postagens = new ArrayList<>();
